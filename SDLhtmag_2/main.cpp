935da6cd0b106964aa6af6107b85ca954a772a3b
thinhkieu725 <thinhkieu725@gmail.com>
2023-03-14 21:42:43 +0700

    Initial Commit

---
+++ SDLhtmag_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDLhtmag_2
+//
+//  Created by Thinh Kieu on 14/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
